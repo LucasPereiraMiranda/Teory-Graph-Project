d4d8fc1234db4fbaa35e59b7f6e17694de7f05ff
Lucas Pereira Miranda <39305813+LucasPereiraMiranda@users.noreply.github.com>
2019-01-30 15:27:21 -0200

    Update Reader.java

--- handling_files/Reader.java
+++ handling_files/Reader.java
@@ -24,36 +24,33 @@ public class Reader{
 		m_GroupEdge = new LinkedHashSet<Edge>();
 	}
 	
-	
-	
 	@SuppressWarnings("resource")
 	public Graph readerDimacs() throws Exception {
 		FileReader v_FileReader = new FileReader(m_FileName);
 		BufferedReader v_Reader = new BufferedReader(v_FileReader);
-	    int i=0;
-	    for(;(m_Row = v_Reader.readLine())!=null;) {
-	    	if(i==0) {
-	    		i++;
-	    		continue;
-	    	}	
-	    	else {
-	    		m_DataInLine = m_Row.split(" ");
-	    		
-	    		this.m_GroupVertice.add(new Vertice(Integer.parseInt(m_DataInLine[0])));
-	    		this.m_GroupVertice.add(new Vertice(Integer.parseInt(m_DataInLine[1])));
-	  
-	    		if(m_IsDirect==false) {
-	    			this.m_GroupEdge.add(new Edge(new Vertice(Integer.parseInt(m_DataInLine[0])),new Vertice(Integer.parseInt(m_DataInLine[1])),Integer.parseInt(m_DataInLine[2])));
-	    			this.m_GroupEdge.add(new Edge(new Vertice(Integer.parseInt(m_DataInLine[1])),new Vertice(Integer.parseInt(m_DataInLine[0])),Integer.parseInt(m_DataInLine[2])));
-	    		}
+	    	int i=0;
+	    	for( ;(m_Row = v_Reader.readLine())!=null; ) {
+	    		if(i==0) {
+	    			i++;
+	    			continue;
+	    		}	
 	    		else {
-	    			this.m_GroupEdge.add(new Edge(new Vertice(Integer.parseInt(m_DataInLine[0])),new Vertice(Integer.parseInt(m_DataInLine[1])),Integer.parseInt(m_DataInLine[2])));
+	    			m_DataInLine = m_Row.split(" ");
+	    		
+	    			this.m_GroupVertice.add(new Vertice(Integer.parseInt(m_DataInLine[0])));
+	    			this.m_GroupVertice.add(new Vertice(Integer.parseInt(m_DataInLine[1])));
+	  
+	    			if(m_IsDirect==false) {
+	    				this.m_GroupEdge.add(new Edge(new Vertice(Integer.parseInt(m_DataInLine[0])),new Vertice(Integer.parseInt(m_DataInLine[1])),Integer.parseInt(m_DataInLine[2])));
+	    				this.m_GroupEdge.add(new Edge(new Vertice(Integer.parseInt(m_DataInLine[1])),new Vertice(Integer.parseInt(m_DataInLine[0])),Integer.parseInt(m_DataInLine[2])));
+	    			}
+	    			else {
+	    				this.m_GroupEdge.add(new Edge(new Vertice(Integer.parseInt(m_DataInLine[0])),new Vertice(Integer.parseInt(m_DataInLine[1])),Integer.parseInt(m_DataInLine[2])));
 	    		}
 	    	}
 	    	i++;
 	    }
 	    m_AuxGraph = new Graph(m_GroupVertice,m_GroupEdge,m_IsDirect);
-		return m_AuxGraph;
+	    return m_AuxGraph;
 	}
-	
 }
